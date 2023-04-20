4ffd4bed90cc897280cf414000e4ebf56f87f2d0
Jourdan Diskin <jourdandiskin@gmail.com>
2023-04-20 10:51:21 -0400

    test rewrite

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,29 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: "subdomain.anj.today",
+            },
+          ],
+          destination: "/downloads/:path*",
+        },
+      ],
+    };
+  },
+  redirects() {
+    return [
+      {
+        source: "/home",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
