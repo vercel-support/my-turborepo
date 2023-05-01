d12e06344c7697d1179e9a3e245fa30dfd79b9b0
Jourdan Diskin <jourdandiskin@gmail.com>
2023-05-01 10:11:12 -0400

    update redirect to rewrite

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,19 +2,20 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
   
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "downloads.anj.today",
-          },
-        ],
-        permanent: true,
-        destination: "/downloads/:path*",
-      },
-    ]
-  },
+  rewrites() {
+    return {
+        beforeFiles: [
+            {
+              source: "/:path*",
+              has: [
+                {
+                  type: "host",
+                  value: "downloads.anj.today",
+                },
+              ],
+              destination: "/downloads/:path*",
+            },
+        ]
+    }
+}
 };
